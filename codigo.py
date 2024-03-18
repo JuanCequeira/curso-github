a1028f4c507107437b996c5c937081b414ed16bc
Juan Cruz Cequeira <95389506+JuanCequeira@users.noreply.github.com>
2024-03-18 11:37:26 -0300

    Subiendo el archivo de Python

---
+++ codigo.py
@@ -0,0 +1,6 @@
+nombre = "Juan Cruz"
+apellido = "Cequeira"
+
+saludo = 'Hola' + nombre + ' ' + apellido + ', como estas?'
+frase_aliento = '. Segui asi estudiando, pronto va a llegar la oportunidad'
+print(saludo, frase_aliento)
